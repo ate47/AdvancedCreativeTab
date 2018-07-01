ddb19fc3aa8c2f584d456f186c79ecfcb780e110
Antoine Willerval <1580223+ate47@users.noreply.github.com>
2018-07-01 20:11:46 +0200

    downgrade to 1.12.1

---
+++ src-1.12.1/main/java/fr/atesab/act/gui/modifier/nbtelement/NBTDoubleElement.java
@@ -0,0 +1,27 @@
+package fr.atesab.act.gui.modifier.nbtelement;
+
+import fr.atesab.act.gui.modifier.GuiListModifier;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagDouble;
+
+public class NBTDoubleElement extends NBTNumericElement<Double> {
+
+	public NBTDoubleElement(GuiListModifier<?> parent, String key, Double value) {
+		this(parent, key, 200, 21, value);
+	}
+
+	public NBTDoubleElement(GuiListModifier<?> parent, String key, int sizeX, int sizeY, Double value) {
+		super("double", parent, key, sizeX, sizeY, value);
+	}
+
+	@Override
+	public NBTBase get(Double value) {
+		return new NBTTagDouble(value);
+	}
+
+	@Override
+	public Double parseValue(String text) {
+		return text.isEmpty() ? 0 : Double.parseDouble(text);
+	}
+
+}
